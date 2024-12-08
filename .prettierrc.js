3bab8717c29428240031af869386fdd1be055ef7
ortima <account.net@mail.ru>
2024-12-09 00:58:41 +0300

    🎉 Created testing project via Vite

---
+++ .prettierrc.js
@@ -0,0 +1,13 @@
+/**
+ * @see https://prettier.io/docs/en/configuration.html
+ * @type {import("prettier").Config}
+ */
+const config = {
+  trailingComma: "es5",
+  tabWidth: 2,
+  semi: true,
+  singleQuote: false,
+  printWidth: 80,
+};
+
+export default config;
